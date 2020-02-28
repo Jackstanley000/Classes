9b7309694274ddd59cf9d923efa0b53c1e5d36ea
Jack Stanley <jackstanley@amdg.rockhursths.edu>
2020-02-28 08:42:19 -0600

    Planetarium

---
+++ planetarium.py
@@ -0,0 +1,16 @@
+from planet import planet
+
+class planetarian(object):
+    def __init__(self):
+        self.create()
+
+    def __str__(self):
+        return '[' + ', '.join(str(planet) for planet in self.planet) + ']'
+
+    def create(self):
+        self.planet = []
+        for i in range(4):
+            for j in range(0,4):
+                self.planet.append(Planet(i,j))
+
+    Planetarium.py
